f8f6963a13a5d0ce045f6efc5620012430664181
Jamie Nicholson <jamiel.nicholson@gmail.com>
2017-10-22 22:19:12 +1000

    Up to Routing

--- APM/src/app/app.module.ts
+++ APM/src/app/app.module.ts
@@ -1,17 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ProductListComponent } from './products/product-list.component';
+import { ConvertToSpacesPipe } from './shared/convert-to-spaces.pipe';
+import { StarComponent } from './shared/star.component';
+import { ProductDetailComponent } from './products/product-detail.component';
+import { WelcomeComponent } from './home/welcome.component';
 
 @NgModule({  // Decorator to defines the below class as an Angular Module and passing the metadata defining the details of this module
   // Properties:
   declarations: [ 
     AppComponent,
-    ProductListComponent
+    ProductListComponent,
+    ConvertToSpacesPipe,
+    StarComponent,
+    ProductDetailComponent,
+    WelcomeComponent
     ],  // Declares the AppComponent so that Angular can locate its selector
-  imports: [ BrowserModule ],      // Imports the Browser Module so that the app runs correctly in the browser
+  imports: [ BrowserModule,
+             FormsModule,
+             HttpClientModule,
+             RouterModule.forRoot([
+                { path: 'products', component: ProductListComponent },
+                { path: 'products/:id', component: ProductDetailComponent },
+                { path: 'welcome', component: WelcomeComponent },
+                { path: '', redirectTo: 'welcome', pathMatch: 'full' },
+                { path: '**', redirectTo: 'welcome', pathMatch: 'full' }
+             ])
+    ],      // Imports the Browser Module so that the app runs correctly in the browser
   providers: [],                   // For Services
-  bootstrap: [AppComponent]        // The starting componant for our application
+  bootstrap: [ AppComponent]         // The starting componant for our application
 })
 export class AppModule { }    // The Class
